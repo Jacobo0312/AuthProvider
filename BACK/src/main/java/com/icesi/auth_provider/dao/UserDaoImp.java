ecf5318d2343c36d4c9ac297b97ca75d7ea79d5e
Jacobo Garcia <72984810+Jacobo0312@users.noreply.github.com>
2023-05-18 16:53:39 -0500

    Add structure for change password

--- BACK/src/main/java/com/icesi/auth_provider/dao/UserDaoImp.java
+++ BACK/src/main/java/com/icesi/auth_provider/dao/UserDaoImp.java
@@ -3,13 +3,14 @@
 package com.icesi.auth_provider.dao;
 
 import com.icesi.auth_provider.models.UserModel;
-import com.google.common.hash.Hashing;
+import com.icesi.auth_provider.utils.HashPBKDF2;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.PersistenceContext;
 import javax.persistence.EntityManager;
-import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 
 @Repository
@@ -40,9 +41,14 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public UserModel changePassword(UserModel user) {
-        String hash = Hashing.sha256()
-                .hashString(user.getPassword(), StandardCharsets.UTF_8)
-                .toString();
+        String hash = null;
+        try {
+            hash = HashPBKDF2.generateStrongPasswordHash(user.getPassword());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
 
         String query = "FROM UserModel WHERE username = :username";
 
@@ -51,7 +57,7 @@ public class UserDaoImp implements UserDao {
                 .getResultList();
 
         if (!users.isEmpty()) {
-             user = users.get(0);
+            user = users.get(0);
             user.setPassword(hash);
             entityManager.persist(user);
             return user;
@@ -74,9 +80,14 @@ public class UserDaoImp implements UserDao {
             throw new IllegalArgumentException("El nombre de usuario ya está en uso");
         }
 
-        String hash = Hashing.sha256()
-                .hashString(user.getPassword(), StandardCharsets.UTF_8)
-                .toString();
+        String hash = null;
+        try {
+            hash = HashPBKDF2.generateStrongPasswordHash(user.getPassword());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
 
         user.setPassword(hash);
         user.setLastLogin(new java.util.Date());
@@ -86,28 +97,34 @@ public class UserDaoImp implements UserDao {
     }
 
 
-
     @Override
     public UserModel login(UserModel user) {
-        String hash = Hashing.sha256()
-                .hashString(user.getPassword(), StandardCharsets.UTF_8)
-                .toString();
 
-        String query = "FROM UserModel WHERE username = :username AND password = :password";
+        String password = user.getPassword();
+
+        String query = "FROM UserModel WHERE username = :username";
 
         List<UserModel> users = entityManager.createQuery(query)
                 .setParameter("username", user.getUsername())
-                .setParameter("password", hash)
                 .getResultList();
-        
+
+        boolean validation = false;
+
         user = !users.isEmpty() ? users.get(0) : null;
 
         if (user != null) {
+            try {
+                validation = HashPBKDF2.validatePassword(password, user.getPassword());
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidKeySpecException e) {
+                throw new RuntimeException(e);
+            }
             user.setLastLogin(new java.util.Date());
             entityManager.persist(user);
         }
 
-        return user;
+        return validation ? user : null;
     }
 
 
--- FRONT/src/pages/Home.tsx
+++ FRONT/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { User } from "../interfaces/User";
+import { Modal } from "@mui/material";
 
 import dayjs from "dayjs";
+import { useState } from "react";
 
 const formatLastLogin = (date: Date) => {
   const formattedDate = dayjs(date).format("YYYY-MM-DD HH:mm");
@@ -8,6 +10,16 @@ const formatLastLogin = (date: Date) => {
 };
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const userString = localStorage.getItem("user");
   const user: User | null = userString ? JSON.parse(userString) : null;
 
@@ -20,6 +32,10 @@ const Home = () => {
         <div>
           <p>Username: {user.username}</p>
           <p>Last Login: {lastLogin}</p>
+          <button onClick={handleOpen}>Cambiar contraseña</button>
+          {/* <Modal open={open} onClose={handleClose}>
+             //Agregar modal que pida la contraseña y la nueva
+          </Modal> */}
         </div>
       )}
     </div>
