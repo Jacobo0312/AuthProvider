4aced5505142cc3ea79bd238040f058dc513fb2f
juanosorio0219 <juanjo9464@gmail.com>
2023-05-18 17:49:21 -0500

    Commit

--- FRONT/src/pages/Home.tsx
+++ FRONT/src/pages/Home.tsx
@@ -1,4 +1,9 @@
 import { User } from "../interfaces/User";
+import '../styles/home.css';
+import { Button } from "@mui/material";
+import {TextField} from "@mui/material";
+import React from "react";
+
 
 import dayjs from "dayjs";
 
@@ -7,19 +12,38 @@ const formatLastLogin = (date: Date) => {
   return formattedDate;
 };
 
+
+
 const Home = () => {
+  const [showTextFields, setShowTextFields] = React.useState(false);
+
+  const handleButtonClick = () => {
+    setShowTextFields(true);
+  };
+
   const userString = localStorage.getItem("user");
   const user: User | null = userString ? JSON.parse(userString) : null;
 
   const lastLogin = user ? formatLastLogin(user.lastLogin!) : null;
 
   return (
-    <div>
+    <div className="Home">
       <h1>Home</h1>
       {user && (
         <div>
           <p>Username: {user.username}</p>
           <p>Last Login: {lastLogin}</p>
+          <div className="Buttons-div">
+          <Button variant="contained" onClick={handleButtonClick}>Change Password</Button>
+          <span className="Button-spacing"></span>
+          <Button variant="contained" color="error">Log out</Button>
+          </div>
+          {showTextFields && (
+            <div className="TextFields-container">
+              <TextField label="Current password" />
+              <TextField label="New password" />
+            </div>
+          )}
         </div>
       )}
     </div>
